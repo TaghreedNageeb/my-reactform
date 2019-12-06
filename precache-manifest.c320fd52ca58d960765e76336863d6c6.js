a30611224351241f2d7a0be39bfa6b9e5a48b44b
Taghreed Najib <taghreed.n.d2018@gmail.com>
2019-12-06 22:45:47 +0300

    Updates

---
+++ precache-manifest.c320fd52ca58d960765e76336863d6c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913352d99781b99777fef893e67fb0b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "730870ba3ed225f0024c",
+    "url": "/static/css/main.550d1fb6.chunk.css"
+  },
+  {
+    "revision": "c3e2c912770e41b25243",
+    "url": "/static/js/2.65989a3f.chunk.js"
+  },
+  {
+    "revision": "730870ba3ed225f0024c",
+    "url": "/static/js/main.3bc4bdd2.chunk.js"
+  },
+  {
+    "revision": "2ffaf4ba443da19904ad",
+    "url": "/static/js/runtime-main.f1fe54bf.js"
+  }
+]);
